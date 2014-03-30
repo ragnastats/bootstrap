0fe98faa817b73eebb31b694338256182ed4fdb7
Rachel Fish <itsrachelfish@gmail.com>
2014-03-30 01:25:33 -0600

    Basic inventory window

---
+++ ragnarok-bootstrap.js
@@ -0,0 +1,24 @@
+$(document).ready(function()
+{
+    // Auto-correct the content's margin based on sidebar and footer
+    $('.ragnarok-window, .ro-win').each(function()
+    {
+        if($(this).find('.ragnarok-sidebar, .ro-side').length)
+        {
+            var sidebar = {
+                width: $(this).find('.ragnarok-sidebar, .ro-side').outerWidth(true),
+                height: $(this).find('.ragnarok-sidebar, .ro-side div').outerHeight(true),
+            };
+
+            console.log(sidebar);
+
+           $(this).find('.ragnarok-window-content, .ro-win-content').css({'padding-left': sidebar.width + 4, 'min-height': sidebar.height});
+        }
+
+        if($(this).find('.ragnarok-window-footer, .ro-win-foot').length)
+        {
+           var footerHeight = $(this).find('.ragnarok-window-footer, .ro-win-foot').outerHeight(true);
+           $(this).find('.ragnarok-window-content, .ro-win-content').css({'padding-bottom': footerHeight + 4});
+        }
+    });
+});
