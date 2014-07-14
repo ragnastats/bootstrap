d5eeecd895c8fc9670892c5c220614b0c8fa7a67
Rachel Fish <itsrachelfish@gmail.com>
2014-07-14 11:14:54 -0600

    Basic hogan template integration

--- js/ragnarok-window.js
+++ js/ragnarok-window.js
@@ -1,21 +1,37 @@
 if(typeof ragnarok !== "undefined")
 {
-	ragnarok.window =
-	{
-		create: function(pos)
-		{
-			// Create a new window!
-		},
-		
-		minimize: function()
-		{
-			// Hide window content
-			// Swap content if necessary
-		},
-		
-		close: function()
-		{
-			// Style the window invisible
-		}
-	}
+    ragnarok.window =
+    {
+        compiled: {},
+        
+        create: function(template, data)
+        {
+            $.get(template, function(response)
+            {
+                var compiled = Hogan.compile(response);
+                var rendered = compiled.render(data);
+                
+                ragnarok.window.compiled[template] = compiled;
+                $('body').append(rendered);
+            });
+        },
+
+        update: function(template, data)
+        {
+            
+        },
+        
+        minimize: function()
+        {
+            // Hide window content
+            // Swap content if necessary
+        },
+        
+        close: function()
+        {
+            // Style the window invisible
+        }
+    }
+    
+    ragnarok.window.create('../windows/test.html', {'test-title': "Hello World!", 'test-content': "Templating systems sure are fun ^_~"});
 }
