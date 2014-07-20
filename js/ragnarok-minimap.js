da55844c85c462db1119212200698d5a4de34eb5
Rachel Fish <itsrachelfish@gmail.com>
2014-07-20 15:54:53 -0600

    Dynamic map resizing since maps are different sizes now

--- js/ragnarok-minimap.js
+++ js/ragnarok-minimap.js
@@ -3,18 +3,41 @@ if(typeof ragnarok !== "undefined")
     ragnarok.minimap =
     {
         name: '',
+        max: {height: 126, width: 126},
         size: {},
         
         init: function(map)
-        {
+        {            
             // Clear previous icons
             ragnarok.minimap.clear();
+            var $map = $('.ro-minimap img');
             
             ragnarok.minimap.name = map.name;
             ragnarok.minimap.size = {height: map.height, width: map.width};
+            ragnarok.minimap.resize(map);
             
             var map_image = "http://cdn.ragnastats.com/map/"+map.name+".png";
-            $('.ro-minimap img').attr('src', map_image);
+            $map.attr('src', map_image);
+        },
+        
+        resize: function(map)
+        {
+            var $map = $('.ro-minimap img');
+            var adjusted = {};
+            
+            // Use whichever side is bigger
+            if(map.height > map.width)
+            {
+                adjusted.height = ragnarok.minimap.max.height;
+                adjusted.width = (adjusted.height * map.width) / map.height;
+            }
+            else
+            {
+                adjusted.width = ragnarok.minimap.max.width;
+                adjusted.height = (adjusted.width * map.height) / map.width;
+            }
+            
+            $map.css({'height': adjusted.height, 'width': adjusted.width, 'opacity': 1});
         },
         
         add: function(pos)
@@ -27,14 +50,14 @@ if(typeof ragnarok !== "undefined")
 
             // Get current map size
             var current_size = {height: $('.ro-minimap').height(), width: $('.ro-minimap').width()};
-            
-            // Adjust position accordingly
+                        
+            // Position icon accordingly
             var icon_pos = 
             {
                 x: (current_size.width / native_size.width) * pos.x,
                 y: (current_size.height / native_size.height) * pos.y
             }
-            
+  
             square.css({bottom: icon_pos.y - 2, left: icon_pos.x - 2});
             $('.ro-minimap').append(square);
         },
